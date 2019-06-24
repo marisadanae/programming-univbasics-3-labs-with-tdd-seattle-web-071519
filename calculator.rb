eab5b5c1a55352b41f307ce237656d7d4d496b29
danaerodriguez96 <danaerodriguez96@gmail.com>
2019-06-24 21:26:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,9 @@
 # Add your variables here
 first_number = 0
 
-second_number = expect(second_number).not_to equal(0)
+expect(second_number).not_to equal(0)
 
-sum = '#{first_number} + #{second_number}'
+puts '#{first_number} + #{second_number}'
 
 first_number(0)
 second_number(1.5)
